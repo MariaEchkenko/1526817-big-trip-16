15da32ebc6f44c16897c8bbdafb84ae435ecbb23
Maria Echkenko <mashasly@mail.ru>
2021-12-13 20:16:42 +0300

    Добавляет закрытие формы по Esc и стрелке

--- src/main.js
+++ src/main.js
@@ -39,13 +39,28 @@ const renderPoint = (listPointsElement, point) => {
     listPointsElement.replaceChild(pointComponent.element, pointEditComponent.element);
   };
 
+  const onEscKeyDown = (evt) => {
+    if (evt.key === 'Escape' || evt.key === 'Esc') {
+      evt.preventDefault();
+      replaceFormToPoint();
+      document.removeEventListener('keydown', onEscKeyDown);
+    }
+  };
+
   pointComponent.element.querySelector('.event__rollup-btn').addEventListener('click', () => {
     replacePointToForm();
+    document.addEventListener('keydown', onEscKeyDown);
+  });
+
+  pointEditComponent.element.querySelector('.event__rollup-btn').addEventListener('click', () => {
+    replaceFormToPoint();
+    document.removeEventListener('keydown', onEscKeyDown);
   });
 
   pointEditComponent.element.querySelector('form').addEventListener('submit', (evt) => {
     evt.preventDefault();
     replaceFormToPoint();
+    document.removeEventListener('keydown', onEscKeyDown);
   });
 
 
