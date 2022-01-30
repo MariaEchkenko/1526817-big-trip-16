f73080a7c22ff0b992981d32636cd3f578102ff5
Maria Echkenko <mashasly@mail.ru>
2022-01-30 17:02:35 +0300

    Реализует функциональность для названия и дат трипа

--- src/view/trip-info-view.js
+++ src/view/trip-info-view.js
@@ -1,24 +1,49 @@
 import AbstractView from './abstract-view.js';
+import {humanizeTaskDate} from '../utils/point.js';
 
 const getTotalPrice = (points) =>
   points.reduce((totalPrice, point) =>
     totalPrice + point.price + point.offers.reduce((totalOffersPrice, {price}) =>
       totalOffersPrice + price, 0), 0);
 
-const createTripInfoTemplate = (points) => {
-  const totalPrice =  getTotalPrice(points);
-  return `<section class="trip-main__trip-info  trip-info">
-    <div class="trip-info__main">
-      <h1 class="trip-info__title">Amsterdam &mdash; Chamonix &mdash; Geneva</h1>
+const getTripDuration = (points) => {
+  const startDate = humanizeTaskDate(points[0].dateFrom, 'D MMM');
+  const endDate = humanizeTaskDate(points[points.length - 1].dateTo, 'D MMM');
+  const durationTrip = `${startDate}&nbsp;&mdash;&nbsp;${endDate}`;
+  return durationTrip;
+};
 
-      <p class="trip-info__dates">Mar 18&nbsp;&mdash;&nbsp;20</p>
+const getTripName = (points) => {
+  const startCity = points[0].destination.name;
+  const secondCity = points[1].destination.name;
+  const endCity = points[points.length - 1].destination.name;
+
+  switch (points.length) {
+    case 1:
+      return startCity;
+    case 2:
+      return `${startCity} &mdash; ${endCity}`;
+    case 3:
+      return `${startCity} &mdash; ${secondCity} &mdash; ${endCity}`;
+    default:
+      return `${startCity} &mdash; ... &mdash; ${endCity}`;
+  }
+};
+
+
+const createTripInfoTemplate = (points) => (
+  `<section class="trip-main__trip-info  trip-info">
+    <div class="trip-info__main">
+      <h1 class="trip-info__title">${getTripName(points)}</h1>
+
+      <p class="trip-info__dates">${getTripDuration(points)}</p>
     </div>
 
     <p class="trip-info__cost">
-      Total: &euro;&nbsp;<span class="trip-info__cost-value">${totalPrice}</span>
+      Total: &euro;&nbsp;<span class="trip-info__cost-value">${getTotalPrice(points)}</span>
     </p>
-  </section>`;
-};
+  </section>`
+);
 
 export default class TripInfoView extends AbstractView {
   #points = null;
